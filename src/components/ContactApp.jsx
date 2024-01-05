6891a5a125d64528b3d397b01bf755befc45d3f2
ikkromm18 <muhammadikkrom749@gmail.com>
2024-01-05 07:05:30 +0700

    memberikan Styling dan memperbaiki Struktur Folder pada src agar lebih tertata

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { getData } from "./data";
+import { getData } from "../../public/utils/data";
 import ContactList from "./ContactList";
 
 function ContactApp() {
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { createRoot } from "react-dom/client";
-import ContactApp from "./ContactApp";
+import ContactApp from "./components/ContactApp";
+import "./styles/style.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<ContactApp />);
